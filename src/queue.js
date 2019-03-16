f9fde8b94957710e20fe709417453fc2dd2bd6d9
Maksim Antonenko <Maksim_Antonenko@epam.com>
2019-03-16 15:53:44 +0400

    feat: implement queue logic

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,27 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if (arguments.length == 1) this.maxSize = maxSize
+		else this.maxSize = 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() >= this.maxSize) throw "Queue overflow";
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.heap.isEmpty()) throw "Queue is empty";
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
